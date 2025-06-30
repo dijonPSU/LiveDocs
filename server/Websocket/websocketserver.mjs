d223b0c0797b45c2ac4ac8311c3c8228a363b7c5
Dijon Miller <dijonmiller@meta.com>
2025-06-30 12:24:51 -0700

    implemented handshake response

--- server/Websocket/websocketserver.mjs
+++ server/Websocket/websocketserver.mjs
@@ -1,6 +1,10 @@
-import { create } from 'domain';
 import { createServer } from 'http';
+import crypto from 'crypto';
+
+
 const PORT = 8080;
+const WEBSOCKET_MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
 
 
 const server = createServer((request, response) => {
@@ -10,6 +14,26 @@ const server = createServer((request, response) => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+
+server.on('upgrade', (request) => {
+  onSocketUpgrade(request);
+});
+
+
+const onSocketUpgrade = (req) => {
+  const webClientSocketKey = req.headers['sec-websocket-key'];
+  console.log(`Received WebSocket connection from ${webClientSocketKey}`);
+  const headers = prepareHandShakeResponse(webClientSocketKey);
+  console.log({headers});
+
+}
+
+// combining the client's key with the magic string, hashing it, and returning a base64-encoded result.
+const prepareHandShakeResponse = (webClientSocketKey) => {
+  const hash = crypto.createHash('sha1').update(webClientSocketKey + WEBSOCKET_MAGIC_STRING);
+  const response =  hash.digest('base64');
+  return response;
+}
 
 //error handling to keep server running
 [
