119c51d93016e5c1fb450ff0e5609b32848eb957
Dijon Miller <dijonmiller@meta.com>
2025-07-07 16:03:02 -0700

    added /me for user auth

--- server/controllers/server.mjs
+++ server/controllers/server.mjs
@@ -51,4 +51,12 @@ app.post("/auth/logout", (req, res) => {
   });
 });
 
+app.get("/me", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.status(401).json({ message: "Not authenticated" });
+  }
+});
+
 export default app;
