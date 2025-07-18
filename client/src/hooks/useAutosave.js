b8cf91595cf1cc6c3c52d1ecc76f59f485fa2523
Dijon Miller <dijonmiller@meta.com>
2025-07-18 14:44:36 -0700

    integrated custom delta logic

--- client/src/hooks/useAutosave.js
+++ client/src/hooks/useAutosave.js
@@ -1,4 +1,6 @@
 import { useRef } from "react";
+import Delta from "quill-delta";
+
 const timeout = 3000;
 
 export default function useAutosave(save, onSuccess, onError, delay = timeout) {
@@ -6,7 +8,16 @@ export default function useAutosave(save, onSuccess, onError, delay = timeout) {
   const latestDelta = useRef(null);
 
   function storeChanges(delta) {
-    latestDelta.current = delta;
+    // construct delta
+    if (!(delta instanceof Delta)) {
+      delta = new Delta(delta);
+    }
+
+    if (latestDelta.current) {
+      latestDelta.current = latestDelta.current.compose(delta);
+    } else {
+      latestDelta.current = delta;
+    }
 
     if (timer.current) clearTimeout(timer.current);
 
