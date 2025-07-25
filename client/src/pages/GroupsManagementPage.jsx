5c97a6a98c4d44d487bf95be17c189c6667b2960
Dijon Miller <dijonmiller@meta.com>
2025-07-25 08:20:54 -0700

    lint update

--- client/src/pages/GroupsManagementPage.jsx
+++ client/src/pages/GroupsManagementPage.jsx
@@ -70,7 +70,7 @@ export default function GroupsManagementPage() {
   };
 
   const handleBackToDocument = () => {
-    navigate("/DocumentPage", { state: { documentId: documentId } });
+    navigate(`/documents/${documentId}`);
   };
 
   if (selectedGroup) {
